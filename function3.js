22b30dea0908bf0566dabda7ed4a174e82588b04
mechimer1972 <126636790+mechimer1972@users.noreply.github.com>
2023-03-01 15:40:39 -0300

    Add files via upload
    Proyecto final CV Argentina Programa 4.0

---
+++ function3.js
@@ -0,0 +1,24 @@
+
+document.getElementById('DNI').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "38.645.176";">Haz Click Aquí"
+};
+
+document.getElementById('EDAD').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo1").innerHTML = "28";">Haz Click Aquí"
+
+};
+
+document.getElementById('CIUDAD').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo2").innerHTML = "Vicente López";">Haz Click Aquí"
+
+};
+
+document.getElementById('CONTACTO').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo3").innerHTML = "11-6098-8768|alfbenag@yahoo.com";">Haz Click Aquí"
+
+};
+
